a02d5b344b9d119de49f24332b6d1d50d8b93917
Mohammad Hammas <hammasansari641@gmail.com>
2025-09-17 22:33:36 +0530

    Update backend API and ingestion files

--- backend/khan_vector/api/main.py
+++ backend/khan_vector/api/main.py
@@ -69,7 +69,8 @@ app = FastAPI(
 
 # CORS setup
 origins = [
-   "https://brofessor-three.vercel.app"
+   "https://brofessor-three.vercel.app",
+   
 ]
 
 app.add_middleware(
--- backend/khan_vector/ingest.py
+++ backend/khan_vector/ingest.py
@@ -41,7 +41,7 @@ embeddings = OpenAIEmbeddings(
 )
 
 # ------------------- Text Splitter -------------------
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=1000)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
 
 # ------------------- Load Previously Uploaded Files -------------------
 if UPLOADED_LOG.exists():
